55fbf2438bcf66e7761f967080097eb67f45b6c3
Maria <galatea996@gmail.com>
2023-08-01 13:27:46 +0300

    Update

--- src/Hello_name.java
+++ src/Hello_name.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Hello_name {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Inter a namber: ");
+        System.out.print("Inter a name: ");
         String name = scanner.nextLine();
         System.out.print("Hello: " + name);
     }
