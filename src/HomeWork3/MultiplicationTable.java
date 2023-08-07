02f142049c130935200cf20b2b2c097c1d57cb0c
Marina Kotova <101731288+Galatea996@users.noreply.github.com>
2023-08-07 14:04:56 +0300

    Add files via upload

---
+++ src/HomeWork3/MultiplicationTable.java
@@ -0,0 +1,37 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("2 * " + i + " = " + 2 * i);
+        }
+        System.out.println(" ");
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("3 * " + i + " = " + 3 * i);
+        }
+
+        System.out.println(" ");
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("4 * " + i + " = " + 4 * i);
+        }
+        System.out.println(" ");
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("5 * " + i + " = " + 5 * i);
+        }
+        System.out.println(" ");
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("6 * " + i + " = " + 6 * i);
+        }
+        System.out.println(" ");
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("7 * " + i + " = " + 7 * i);
+        }
+        System.out.println(" ");
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("8 * " + i + " = " + 8 * i);
+        }
+        System.out.println(" ");
+        for (int i = 2; i <= 10; i++) {
+            System.out.println("9 * " + i + " = " + 9 * i);
+        }
+    }
+
+}
